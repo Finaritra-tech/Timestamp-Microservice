d00873bd987058cb7d48bfe567b4fe4adb0592c3
unknown <stangwen4@gmail.com>
2025-10-27 16:14:40 +0100

    Repo updated

--- index.js
+++ index.js
@@ -24,26 +24,33 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/:date?", (req, res) => {
-  let dateInput = req.params.date
+app.get("/api", (req, res) => {
+  const now = new Date();
+  res.json({
+    unix: now.getTime(),
+    utc: now.toUTCString()
+  });
+});
 
-  if(!dateInput){
-    const now = new Date()
-    return res.json({unix : now.getTime(), utc: now.toUTCString()})
+app.get("/api/:date", (req, res) => {
+  let dateInput = req.params.date;
+
+  if (!isNaN(dateInput)) {
+    dateInput = parseInt(dateInput);
   }
 
-  if(!isNaN(dateInput)){
-    dateInput = parseInt(dateInput)
-  }
-  const date = new Date(dateInput)
+  const date = new Date(dateInput);
 
-  if (date.toString() === "Invalid Date"){
-    return  res.json({error : "Invalid Date" })
+  if (date.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
   }
 
-  res.json({unix : date.getTime(), utc: date.toUTCString()})
-}
-)
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
